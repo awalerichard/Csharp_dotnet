2e1f6ee0700cd3bb3817f2fb933018990024e8d6
Richard Awale <richardawale182@gmail.com>
2022-05-07 14:24:52 +0300

    initial commit, set uped web application proeject

---
+++ Integirfy_Demo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Integirfy_Demo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
